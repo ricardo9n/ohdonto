be46addf185140340f2bad99aea117852e61240d
Ricardo Rubens <ricardo.rubens@gmail.com>
2022-03-19 21:10:47 -0300

    aula 2 ok

--- lib/app/pages/main_page.dart
+++ lib/app/pages/main_page.dart
@@ -11,20 +11,30 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPagePageState extends State<MainPage> {
-  late Appointment _appointment;
+  late Appointment _appointment, _app2;
 
   @override
   void initState() {
     super.initState();
 
     _appointment = Appointment(
-      patient: Patient('profile.png', 'RR'),
+      patient: Patient('assets/images/profile.png', 'RR'),
       description: 'Canal dentário',
       appointmentTime: AppointmentTime(
         DateTime(2022, 3, 20, 10, 30),
         DateTime(2022, 3, 20, 11, 0),
       ),
       isConfirmed: true,
+    );
+  
+    _app2 = Appointment(
+      patient: Patient('assets/images/profile.png', 'RR'),
+      description: 'Canal dentário',
+      appointmentTime: AppointmentTime(
+        DateTime(2022, 3, 20, 10, 30),
+        DateTime(2022, 3, 20, 11, 0),
+      ),
+      isConfirmed: false,
     );
   }
 
@@ -39,22 +49,76 @@ class _MainPagePageState extends State<MainPage> {
                   children: [
                     _buildFirsLine(),
                     _buildSecondLine(),
+                    _buildAppointment(_appointment),
+                    _buildAppointment(_app2),
                     _buildAppointment(_appointment)
                   ],
                 ))));
   }
 
   Widget _buildAppointment(Appointment appointment) {
-    return _buildPatientRequestList(appointment);
+    return Padding(
+      padding: const EdgeInsets.only(top: 8, bottom: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildConfirmed(appointment),
+          const SizedBox(
+            width: 12,
+          ),
+          _buildProfilePicture(appointment),
+          const SizedBox(
+            width: 12,
+          ),
+          Expanded(child: _buildPatientRequestList(appointment)),
+          _buildCircledArrow(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCircledArrow() {
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.blue),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          Icons.arrow_forward_ios_outlined,
+          size: 15,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildConfirmed(Appointment appointment) {
+    return Container(
+        width: 5,
+        height: 50,
+        color: appointment.isConfirmed ? Colors.green : Colors.red);
+  }
+
+  Widget _buildProfilePicture(Appointment appointment) {
+    final String profile = appointment.patient.profilePicturePath;
+    return CircleAvatar(
+      radius: 20,
+      backgroundImage: AssetImage(profile),
+    );
   }
 
   Widget _buildPatientRequestList(Appointment appointment) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(appointment.patient.name, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-        Text(appointment.description+"X"),
-        Text(appointment.formatAppointmentTime()),
+        Text(
+          appointment.patient.name,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        Text(appointment.description),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Text(appointment.formatAppointmentTime()),
+        ),
       ],
     );
   }
