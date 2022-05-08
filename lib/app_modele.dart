5a6c6553645fba50a8c469e65044bc8087040a7a
Ricardo Rubens <ricardo.rubens@gmail.com>
2022-05-08 09:40:34 -0300

    modulo 7, aula 2

--- lib/app_modele.dart
+++ lib/app_modele.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ohdonto/get_started/onboarding_screen_page.dart';
+import 'package:ohdonto/signin_signup/signin_signup_module.dart';
+import 'routes.dart';
 
 class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
         //ModuleRoute('/$toGetStartedPage', module: GetStartedModule()),
         ChildRoute('/', child: (context, args) => OnBoardingScreenWidget()),
+        ModuleRoute('/$toGetStartedPage', module: SignInSignUpModule()),
       ];
 }
